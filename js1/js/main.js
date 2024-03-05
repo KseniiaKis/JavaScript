b2aa50384cfcc3b5e5a8ee39880396cf4c681be9
KseniiaKis <99728814+KseniiaKis@users.noreply.github.com>
2024-03-05 12:13:02 +0300

    Add files via upload
    js1

--- js1/js/main.js
+++ js1/js/main.js
@@ -5,19 +5,15 @@ let numberOfFilms;
 numberOfFilms = prompt( "Сколько фильмов вы уже посмотрели?", "");
 
 const personalMovieDB = {
-	count: 0,
+	count: numberOfFilms,
 	movies: {},
 	actors: {},
 	genres: [],
 	privat: false
 };
 
-personalMovieDB.count = numberOfFilms;
-
-const movies = {};
-
 for(let i=1; i < 3; i++) {
-  movies[i + prompt( "Один из последних просмотренных фильмов?", "")] = prompt( "На сколько оцените его?", "");
+  personalMovieDB.movies[i + prompt( "Один из последних просмотренных фильмов?", "")] = prompt( "На сколько оцените его?", "");
 };
 
-console.log(personalMovieDB, movies);
+console.log(personalMovieDB);
