5fd779ba12f0dc26f1420fb19b002b2900a13e36
KseniiaKis <99728814+KseniiaKis@users.noreply.github.com>
2024-03-05 13:35:28 +0300

    Add files via upload
    js2

---
+++ js2/js2/js/main.js
@@ -0,0 +1,39 @@
+"use strict"
+
+let numberOfFilms = prompt( "Сколько фильмов вы уже посмотрели?", "");
+
+while (numberOfFilms == "" || numberOfFilms == null || numberOfFilms.length > 50 ) {
+	numberOfFilms = prompt( "Сколько фильмов вы уже посмотрели?", "");
+}
+
+const personalMovieDB = {
+	count: Number(numberOfFilms),
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+if (personalMovieDB.count < 10){
+	console.log('Просмотрено довольно мало фильмов');
+} else if (10 <= personalMovieDB.count && personalMovieDB.count < 30) {
+	console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+	console.log('Вы киноман');
+} else {
+	console.log('Произошла ошибка');
+};
+
+for (let i = 1; i < 3; i++){
+	let a = prompt( "Один из последних просмотренных фильмов?", "");
+	while( a == "" || a == null || a.length > 50 ){
+		a = prompt( "Один из последних просмотренных фильмов?", "");
+	};
+	let b = prompt( "На сколько оцените его?", "")
+	while( b == "" || b == null || b.length > 50 ){
+		b = prompt( "На сколько оцените его?", "")
+	};
+	personalMovieDB.movies[a] = b;
+};
+
+console.log(personalMovieDB);
